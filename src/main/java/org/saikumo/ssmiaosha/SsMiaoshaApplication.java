fec7a6c8ed18e1f24e9c0bad37b78e4046bd39cd
Xufuhao <saikumo11@gmail.com>
2022-06-19 16:18:21 +0800

    项目初始化

---
+++ src/main/java/org/saikumo/ssmiaosha/SsMiaoshaApplication.java
@@ -0,0 +1,13 @@
+package org.saikumo.ssmiaosha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsMiaoshaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsMiaoshaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/saikumo/ssmiaosha/SsMiaoshaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.saikumo.ssmiaosha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsMiaoshaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
